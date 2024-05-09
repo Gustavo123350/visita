a6173da18b5a9488ce910f32052edb806d1e75db
Gustavo123350 <gutafava@outlook.com>
2024-05-08 21:38:08 -0300

    Visitas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const app = createApp(App)
 
-app.use(router)
+const firebaseConfig = {
+    apiKey: "AIzaSyD01gvEs0QwIsVOChtUI2v5boLM6Xn9M10",
+    authDomain: "visita-ab5a6.firebaseapp.com",
+    projectId: "visita-ab5a6",
+    storageBucket: "visita-ab5a6.appspot.com",
+    messagingSenderId: "421634140690",
+    appId: "1:421634140690:web:5eb99bd5a062602ed646cb",
+    measurementId: "G-4FJ5Z47GW8"
+};
 
-app.mount('#app')
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
+const app = createApp(App);
+app.use(router);
+app.use(auth); 
+app.mount('#app');
--- src/views/EuView.vue
+++ src/views/EuView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="card" style="width: 18rem;">
-  <img src="/src/assets/eu.jpeg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Criador do Site</h5>
-    <p class="card-text">Olá meu nome é Gustavo Fava sou criador da empresa "Fava Desenvolvimento",
-      tenho 23 anos, faço o seu sistema ou aplicativos e sites, tiramos sua idea do papel para tela. 
-    </p>
+    <img :src="creatorImage" class="card-img-top" alt="Foto do criador do site">
+    <div class="card-body">
+      <h5 class="card-title">Criador do Site</h5>
+      <p class="card-text">Olá, meu nome é Gustavo Fava.</p>
+      <p class="card-text">Sou criador da empresa "Fava Desenvolvimento", tenho 23 anos.</p>
+      <p class="card-text">Faço o seu sistema ou aplicativos e sites, tiramos sua ideia do papel para tela.</p>
+    </div>
   </div>
-</div>
 </template>
+
+<script>export default {
+  data() {
+    return {
+      creatorImage: '/src/assets/eu.jpeg'
+    }
+  }
+};
+</script>
